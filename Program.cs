d32990554131a727faa3d8650ad39ef26cce29e0
Michelle <michellemarcelo791@gmail.com>
2025-09-26 19:55:23 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string nome, sobrenome;
+Console.Write("dgite seu nome:");
+nome = Console.ReadLine()!;
+Console.Write("digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"seunome completo: {nome} {sobrenome}");
+Console.WriteLine($"seu nome catálogo: {sobrenome.ToUpper()}, {nome}");
